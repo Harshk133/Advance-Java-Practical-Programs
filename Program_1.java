cf3e11760f0db652f2ae94cc0d1603e2e27e1a1e
Harsh <harshmkale.2004@gmail.com>
2023-09-26 08:57:26 +0530

    #7

--- Program_1.java
+++ Program_1.java
@@ -1,13 +1,32 @@
-// Here's a basic example that shows how to create a Swing label with the text "Hello from Swing!"
-
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
-public class Program_1 extends JFrame {
-    public Program_1() {
-        Container contentPane = getContentPane();
-        JLabel jlabel = new JLabel("Hello From Swing!");
-        contentPane.setLayout(new FlowLayout());
-        contentPane.add(jlabel);
+public class Program_1 extends JFrame implements ActionListener {
+    // Globally Declared Variables!
+    TextField tf1, tf2;
+    Button btn1;
+    Program_1(){
+        setLayout(new FlowLayout());
+        tf1 = new TextField(15);
+        tf2 = new TextField(15);
+        btn1 = new Button("Copy");
+        tf2.setEditable(false);
+        add(tf1);
+        add(btn1);
+        add(tf2);
+        btn1.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        String s = tf1.getText();
+        tf2.setText(s);
+    }
+
+    public static void main(String[] args) {
+        Program_1 h = new Program_1();
+        h.setSize(300, 300);
+        h.setTitle("Demonstrating Action Listener");
+        h.setVisible(true);
     }
 }
